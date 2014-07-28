a94819f99e78c5e424474ad5316d848b44ca43b4
WAFcoding <pasquale.verlotta@gmail.com>
2014-07-28 18:27:35 +0200

    aggiunto layout playlist

--- PlayerBoRoVe/src/it/borove/playerborove/MainActivity.java
+++ PlayerBoRoVe/src/it/borove/playerborove/MainActivity.java
@@ -2,13 +2,10 @@
  * Questa classe è la activity principale, quella che viene lanciata all'avvio dell'applicazione
  */
 package it.borove.playerborove;
-import PlayerManager.Duration;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -27,7 +24,6 @@ public class MainActivity extends Activity {
 		settings= (Button)findViewById(R.id.button3);
 		player= (Button)findViewById(R.id.button4);
 		this.addButtonListener();
-
 	}
 	/**
 	 * Aggiunge i listener ai pulsanti
--- PlayerBoRoVe/src/it/borove/playerborove/PlaylistActivity.java
+++ PlayerBoRoVe/src/it/borove/playerborove/PlaylistActivity.java
@@ -1,9 +1,18 @@
 package it.borove.playerborove;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.HorizontalScrollView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class PlaylistActivity extends Activity {
 
@@ -11,5 +20,47 @@ public class PlaylistActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_playlist);
+		
+		//per ogni playlist
+		ArrayList<String> tracks= new ArrayList<String>();
+		for(int i=0; i<10; i++){
+			tracks.add("prova" + i);
+		}
+		configure("", tracks);
+		
+	}
+	
+	public TableRow configure(String img_name, ArrayList<String> tracks){
+		
+		TableRow table_row= new TableRow(this);
+		table_row.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+		
+		LinearLayout row= new LinearLayout(this);
+		row.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+		row.setOrientation(LinearLayout.HORIZONTAL);
+		LinearLayout.LayoutParams img_view= new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT, 0.2f);
+		
+		ImageView img= new ImageView(this);
+		img.setLayoutParams(img_view);
+		int resId= getResources().getIdentifier(img_name, "drawable", getPackageName());
+		img.setImageResource(resId);
+		img.setVisibility(ImageView.VISIBLE);
+
+		LinearLayout.LayoutParams scroll_view= new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT, 0.8f);
+		HorizontalScrollView scroll= new HorizontalScrollView(this);
+		scroll.setLayoutParams(scroll_view);
+		for(String s : tracks){
+			TextView txt= new TextView(this);
+			txt.setText(s);
+			scroll.addView(txt);
+		}
+		
+		row.addView(img);
+		row.addView(scroll);
+		
+		table_row.addView(row);
+		
+		return table_row;
+		
 	}
 }
