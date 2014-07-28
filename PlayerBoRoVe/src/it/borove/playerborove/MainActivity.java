88621d3e090cb19007c22f6f470e75281a8e70b8
WAFcoding <pasquale.verlotta@gmail.com>
2014-07-28 17:07:37 +0200

    aggiunto layout del main activity e inserite le transizioni

--- PlayerBoRoVe/src/it/borove/playerborove/MainActivity.java
+++ PlayerBoRoVe/src/it/borove/playerborove/MainActivity.java
@@ -1,25 +1,78 @@
+/**
+ * Questa classe è la activity principale, quella che viene lanciata all'avvio dell'applicazione
+ */
 package it.borove.playerborove;
-
 import PlayerManager.Duration;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+	
+	private Button playlist, library, settings, player;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-			
-			Duration d= new Duration(13, 59, 55);
-			Log.d("DURATION", "Duration iniziale= " + d.getDuration());
-			
-			d.sum(new Duration(1,20,10));
-			
-			Log.d("DURATION", "Duration finale= " + d.getDuration());
+		
+		playlist= (Button)findViewById(R.id.button1);
+		library= (Button)findViewById(R.id.button2);
+		settings= (Button)findViewById(R.id.button3);
+		player= (Button)findViewById(R.id.button4);
+		this.addButtonListener();
 
+	}
+	/**
+	 * Aggiunge i listener ai pulsanti
+	 */
+	private void addButtonListener(){
+		
+		playlist.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(MainActivity.this, PlaylistActivity.class));
+				//animazione a comparsa da sinistra
+				overridePendingTransition(R.anim.right_in, R.anim.left_out); 
+			}
+		});
+		
+		library.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(MainActivity.this, LibraryActivity.class));
+				//animazione a comparsa da destra
+				overridePendingTransition(R.anim.left_in, R.anim.right_out); 
+			}
+		});
+		
+		settings.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+				//animazione a comparsa da sinistra
+				overridePendingTransition(R.anim.right_in, R.anim.left_out); 
+			}
+		});
+		
+		player.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(MainActivity.this, PlayerActivity.class));
+				//animazione a comparsa dal basso
+				overridePendingTransition(R.anim.bottom_in, R.anim.top_out); 
+				
+			}
+		});
 	}
 }
