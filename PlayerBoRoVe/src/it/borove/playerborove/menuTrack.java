6c3bf41245ad06bd8714cc0dbd939c03ada22d5f
Gringo85 <marco.bonuglia85@gmail.com>
2014-09-12 20:33:33 +0200

    aggiunto un activity menu

---
+++ PlayerBoRoVe/src/it/borove/playerborove/menuTrack.java
@@ -0,0 +1,61 @@
+package it.borove.playerborove;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.method.KeyListener;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class menuTrack extends Activity{
+	private Intent MyCallerIntent;
+	private TextView textViewDetails;
+	private TextView textViewErase;
+
+	private final int RESULT_CODE_DETAILS 	= 300;
+	private final int RESULT_CODE_ERASE		= 310;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.menu_track);
+		
+		textViewDetails 		= (TextView)findViewById(R.id.details);
+		textViewErase 			= (TextView)findViewById(R.id.erase);
+		this.MyCallerIntent 	= getIntent();
+
+		starsListeners();
+		
+	}
+	
+	public void starsListeners(){
+		textViewDetails.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub			
+				setResult(RESULT_CODE_DETAILS, MyCallerIntent);
+				finish();
+			}
+		});
+		
+		textViewErase.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+							
+				setResult(RESULT_CODE_ERASE, MyCallerIntent);
+				finish();
+				
+			}
+			
+		});
+		
+	}
+
+}
