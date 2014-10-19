d11b68fd2bf03d6a7e715e911149b99c445c09f1
pasquale <pasquale.verlotta@gmail.com>
2014-10-19 18:59:52 +0200

    modificati i titoli delle activity, aggiunti dettagli della singola playlist

---
+++ PlayerBoRoVe/src/it/borove/playerborove/PlaylistSingleDetails.java
@@ -0,0 +1,54 @@
+package it.borove.playerborove;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+public class PlaylistSingleDetails extends Activity {
+	
+	private TextView n_track_in_playlist, total_duration,
+					longest_track_in_playlist;
+	private AlertDialog.Builder builder;
+	private Intent MyCallerIntent;
+	private Bundle myBundle;
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_playlist_single_details);
+		
+		n_track_in_playlist= (TextView)findViewById(R.id.etxt_total_track_in_playlist);
+		total_duration= (TextView)findViewById(R.id.etxt_total_single_playlist_duration);
+		longest_track_in_playlist= (TextView)findViewById(R.id.etxt_longest_track_in_playlist);
+		
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+		 
+		builder = new AlertDialog.Builder(PlaylistSingleDetails.this);
+
+		this.MyCallerIntent = getIntent();
+		this.myBundle 	= this.MyCallerIntent.getExtras();
+		
+		n_track_in_playlist.setText(myBundle.getString("n_track_in_playlist"));
+		total_duration.setText(myBundle.getString("total_duration"));
+		longest_track_in_playlist.setText(myBundle.getString("longest_track_in_playlist"));
+		longest_track_in_playlist.setSelected(true);
+	}
+	
+	public boolean onKeyDown(int keyCode, KeyEvent event){
+		
+		if(keyCode == KeyEvent.KEYCODE_BACK){
+
+			finish();
+			overridePendingTransition(R.anim.left_in, R.anim.right_out);
+			return true;
+		}	
+		return super.onKeyDown(keyCode, event); 
+	}
+}
